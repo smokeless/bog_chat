b6aa05aa042609ed5babfad997f91ebd40eaaeb7
Ross Russell <ross.russell.ca@gmail.com>
2018-02-02 18:55:54 -0800

    learning tkinter

--- interface.py
+++ interface.py
@@ -10,45 +10,35 @@ class App(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        chatFrame = tk.Frame(self, relief='ridge', borderwidth=2)
-        listFrame = tk.Frame(self, relief='ridge', borderwidth=2)
-        incomingTextFrame = tk.Frame(self, relief='ridge', borderwidth=2)
+        textEntryFrame       = tk.Frame()
+        channelListFrame     = tk.Frame()
+        incomingMessageFrame = tk.Frame()
 
         #channel list in list frame
         channels = ['Auction', 'Bitch', 'Bogleg', 'Bughouse',
                     'Food', 'Game', 'Glb', 'Gossip', 'Headlines',
                     'Info', 'Linux', 'Meetings', 'Money', 'Newbie',
                     'Radio', 'Sports', 'Tech', 'Vegas']
-        listBoxWidget = tk.Listbox(listFrame) #create wiget, put it in root
+        listBoxWidget = tk.Listbox(channelListFrame) #create wiget, put it in list frame.
         for item in reversed(channels):  #reversed for alaphabetical.
             listBoxWidget.insert(0, item)
-        listBoxWidget.pack(expand = True, fill='y', side='right')
+        listBoxWidget.pack(fill='both', expand=True)
+
+        textBox = tk.Text(textEntryFrame, height = 2, bg='red')
+        label   = tk.Label(textEntryFrame, text='someone@eotl.org')
+        label.pack(side='left')
+        textBox.pack(side='left', fill='x', expand=True)
+
+        sendButton = tk.Button(textEntryFrame, text='Send')
+        sendButton.pack(side='right')
 
 
-        chatBoxWidget = tk.Label(incomingTextFrame, text='text')
-        chatBoxWidget.pack(expand = True, side='top')
-
-        userLabelWidget = tk.Label(chatFrame, text=self.user)
-        userLabelWidget.pack(side='left')
-
-        textEntryWidget = tk.Text(chatFrame, height = 2)
-        textEntryWidget.focus_set()
-        textEntryWidget.config(highlightbackground='RED')
-        textEntryWidget.pack(expand = True, fill='x', side='bottom')
-        chatFrame.pack(expand = True, fill='x', side='bottom')
-        listFrame.pack(expand = True, fill='y', side='right')
-        incomingTextFrame.pack(expand = True, fill='both', side='top')
+        channelListFrame.pack(fill='both', expand=True, side='right')
+        incomingMessageFrame.pack(fill='both', expand=True, side='left')
+        textEntryFrame.pack(side='bottom')
 
 
-        # frame1 = tk.Frame(self, relief='ridge', borderwidth=1)
-        # frame1.pack(fill='both', expand=True)
-        #
-        # self.pack(fill='both', expand=True)
-        #
-        # closeButton = tk.Button(self, text="Close")
-        # closeButton.pack(side='right', padx=5, pady=5)
-        # okButton = tk.Button(self, text="OK")
-        # okButton.pack(side='right')
+
 
 root = tk.Tk()
 root.minsize(width=640, height=480)
