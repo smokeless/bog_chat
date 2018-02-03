2120284087ceaec2835fe8da74a76e1be8979257
Ross Russell <ross.russell.ca@gmail.com>
2018-02-02 19:59:25 -0800

    ugliest interface is a go.

--- interface.py
+++ interface.py
@@ -12,7 +12,6 @@ class App(tk.Frame):
     def create_widgets(self):
         textEntryFrame       = tk.Frame()
         channelListFrame     = tk.Frame()
-        incomingMessageFrame = tk.Frame()
 
         #channel list in list frame
         channels = ['Auction', 'Bitch', 'Bogleg', 'Bughouse',
@@ -24,24 +23,39 @@ class App(tk.Frame):
             listBoxWidget.insert(0, item)
         listBoxWidget.pack(fill='both', expand=True)
 
-        textBox = tk.Text(textEntryFrame, height = 2, bg='red')
-        label   = tk.Label(textEntryFrame, text='someone@eotl.org')
-        label.pack(side='left')
-        textBox.pack(side='left', fill='x', expand=True)
 
-        sendButton = tk.Button(textEntryFrame, text='Send')
-        sendButton.pack(side='right')
+        self.textBox = tk.Text(textEntryFrame, height = 1, bg='red')
+        label   = tk.Label(textEntryFrame, text=self.user)
+        label.pack(side='bottom')
+        self.textBox.pack(side='bottom', fill='x', expand=True)
 
+
+        chatIncoming = tk.Listbox(textEntryFrame)
+        chatIncoming.pack(side='top', fill='both', expand=True)
 
         channelListFrame.pack(fill='both', expand=True, side='right')
-        incomingMessageFrame.pack(fill='both', expand=True, side='left')
-        textEntryFrame.pack(side='bottom')
 
+        textEntryFrame.pack(expand=True, fill='both')
+
+    def get_text_box(self):
+        v = self.textBox.get('1.0', 'end')
+        return v
+    def clear_text_box(self):
+        self.textBox.delete('1.0', 'end')
+
+
+
+def enter_pressed(what):
+    words = app.get_text_box()
+    words = words.strip()
+    app.clear_text_box()
+    print(words)
 
 
 
 root = tk.Tk()
 root.minsize(width=640, height=480)
 root.title('Bog Chatter')
+root.bind('<Return>', enter_pressed)
 app = App(master=root)
 app.mainloop()
